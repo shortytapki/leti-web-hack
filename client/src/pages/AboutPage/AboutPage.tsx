77b9bed0a9e4d05b7a124e3e05e0627861d417aa
shortytapki <v.v.vinogradov99@gmail.com>
2023-11-06 22:00:42 +0300

    updated about page

--- client/src/pages/AboutPage/AboutPage.tsx
+++ client/src/pages/AboutPage/AboutPage.tsx
@@ -1,13 +1,34 @@
-import { classNames } from '@shared/lib';
+import cls from './AboutPage.module.css';
 
 const AboutPage = () => {
   return (
-    <div>
-      <h2>50 Герц - магазин фирменной продукции СПбГЭТУ ЛЭТИ.</h2>
-      <p>
-        Обозначь наличие заряда в своём образе студента вместе с фирменной
-        одеждой и аксессуарами.
-      </p>
+    <div className={cls.AboutPage}>
+      <article>
+        В нашем магазине вы найдете широкий выбор мерча, посвященного ЛЭТИ. От
+        одежды до аксессуаров, мы предлагаем качественные товары с уникальными
+        дизайнами, которые отражают дух и идентичность нашего университета. Будь
+        то футболка с символикой ЛЭТИ, кружка с логотипом или стильная сумка, у
+        нас есть все, чтобы вы могли выразить свою привязанность к ЛЭТИ в любой
+        ситуации.
+      </article>
+      <div>
+        <img
+          className={cls.image}
+          height={300}
+          width={300}
+          src="https://highstore.ru/image/cache/catalog/mockups/27-SPBLETI/art/hoody-932x1400.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className={cls.image}
+          src="https://highstore.ru/image/cache/catalog/mockups/27-SPBLETI/minimal/hoody-932x1400.jpg"
+          width={300}
+          height={300}
+          alt=""
+        />
+      </div>
     </div>
   );
 };
